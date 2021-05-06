93b90f0af72d0e03da079348d032c3a48503ba61
Bruno <brunocbarreto2012@gmail.com>
2021-05-06 08:02:46 -0300

    Configuração do identity

--- AspNetCore.EnterpriseApplication/src/services/Identidade.API/Startup.cs
+++ AspNetCore.EnterpriseApplication/src/services/Identidade.API/Startup.cs
@@ -1,7 +1,10 @@
+using Identidade.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,14 +24,21 @@ namespace Identidade.API
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddControllers();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -40,8 +50,9 @@ namespace Identidade.API
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
--- AspNetCore.EnterpriseApplication/src/services/Identidade.API/obj/Debug/netcoreapp3.1/Identidade.API.AssemblyInfo.cs
+++ AspNetCore.EnterpriseApplication/src/services/Identidade.API/obj/Debug/netcoreapp3.1/Identidade.API.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("094d92cc-fb15-4e3e-bdc3-4dfb16078cb7")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Identidade.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
