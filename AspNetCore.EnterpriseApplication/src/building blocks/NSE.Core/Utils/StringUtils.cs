2fafe78553741b294beebd4a4eaeb81e2343c5ce
Bruno <brunocbarreto2012@gmail.com>
2021-08-19 08:20:04 -0300

    Estruturando API de cliente

---
+++ AspNetCore.EnterpriseApplication/src/building blocks/NSE.Core/Utils/StringUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NSE.Core.Utils
+{
+    public static class StringUtils
+    {
+        public static string ApenasNumeros(this string str, string input)
+        {
+            return new string(input.Where(char.IsDigit).ToArray());
+        }
+    }
+}
---
+++ AspNetCore.EnterpriseApplication/src/services/Cliente.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,19 @@
+﻿using Cliente.API.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cliente.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            //services.AddScoped<IMediatorHandler, MediatorHandler>();
+            //services.AddScoped<IRequestHandler<RegistrarClienteCommand, ValidationResult>, ClienteCommandHandler>();
+
+            //services.AddScoped<INotificationHandler<ClienteRegistradoEvent>, ClienteEventHandler>();
+
+            //services.AddScoped<IClienteRepository, ClienteRepository>();
+            services.AddScoped<ClientesContext>();
+        }
+    }
+}
